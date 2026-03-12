741c41d90bf95d9cade4fc3a5b3ca53f297b4071
Muhammad Bakaye <muhammadbakaye@gmail.com>
2026-03-12 11:02:41 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import DeletedItems from './pages/DeletedItems';
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
